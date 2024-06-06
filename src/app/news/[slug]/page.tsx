1a1cadc9f3b4cac6447752e58a906875838d8291
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-06 19:56:36 -0300

    refactor: change empty tag to div tag, change header tag to Header component, include slug and description props in NewsDetails component and add footer tag

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 import { getTrendingNews } from "@/models/data-fetching/get-trending-news.model";
+
+import { Header } from "@/view/components/header";
 import { NewsDetails } from "@/view/components/news-details";
+import { ArrowIcon } from "@/view/icons/arrow-icon";
 
 export default async function New({ params }: { params: { slug: string } }) {
   const response = await getTrendingNews();
@@ -12,18 +17,26 @@ export default async function New({ params }: { params: { slug: string } }) {
   const details = trendingNews.filter((item) => item.slug === params.slug);
 
   return (
-    <>
-      <header>Oi</header>
+    <div>
+      <Header />
       <NewsDetails
         key={details[0].id}
-        slug={details[0].slug!}
+        slug={details[0].slug ?? ""}
         title={details[0].title}
         author={details[0].author}
-        content={details[0].content!}
-        urlImage={details[0].urlToImage!}
+        content={details[0].content ?? ""}
+        description={details[0].description ?? ""}
+        urlImage={details[0].urlToImage ?? ""}
         publishedAt={details[0].publishedAt}
       />
-      ;
-    </>
+      <footer className="flex justify-between items-center w-full py-12 px-8 md:px-16 lg:px-32 xl:px-64 lg:justify-start lg:gap-16">
+        <Link href={"/"}>
+          <ArrowIcon />
+        </Link>
+        <Link href={"/"} className="hover:underline">
+          Voltar a página inicial
+        </Link>
+      </footer>
+    </div>
   );
 }
