c08f880276c1ca6382e492f93b16416b5b95a3af
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-06 19:59:32 -0300

    refactor: add slug props of the NewsProps and change # to path in href link tag

--- src/view/components/short-news.tsx
+++ src/view/components/short-news.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import dayjs from "dayjs";
 import "dayjs/locale/pt-br";
 
-export function ShortNews({ title, publishedAt }: NewsProps) {
+export function ShortNews({ title, publishedAt, slug }: NewsProps) {
   const dateNews = dayjs(publishedAt, { utc: true });
   const dateNewsFormat = dateNews.locale("pt-br").format("HH:mm");
 
   return (
     <div className="max-w-80">
-      <Link href="#">
+      <Link href={`/news/${slug}`}>
         <span className="cursor-pointer hover:underline">{title}</span>
       </Link>{" "}
       - {dateNewsFormat}
