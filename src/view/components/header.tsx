78783e4051b6e0980b22f630219546ef146a9254
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-06 19:56:47 -0300

    feat: create header component

---
+++ src/view/components/header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 p-8 bg-gray-50 text-center align-middle">
+      <Link href={"/"}>
+        <h1 className="text-2xl">Portal News</h1>
+      </Link>
+    </header>
+  );
+}
