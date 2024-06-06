b05069bc77008fffaead67ed96ef4b1439d94565
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-06 18:39:51 -0300

    refactor: change client to server component, remove useFetchTrendingNews hook, add async funcion, add getTrendingNews function, change idx to item.id in map keys and include slug in map props

--- src/view/components/trending-news.tsx
+++ src/view/components/trending-news.tsx
@@ -1,14 +1,18 @@
-"use client";
-
 import dayjs from "dayjs";
 import "dayjs/locale/pt-br";
 
-import { useFetchTrendingNews } from "../hooks/use-fetch-trending-news";
 import { MainNews } from "./main-news";
 import { ShortNews } from "./short-news";
+import { getTrendingNews } from "@/models/data-fetching/get-trending-news.model";
 
-export function TrendingNews() {
-  const { trendingNews } = useFetchTrendingNews();
+export async function TrendingNews() {
+  const response = await getTrendingNews();
+
+  if (!response) {
+    return null;
+  }
+
+  const trendingNews = response.data.articles;
 
   const mainNews = trendingNews.slice(0, 1);
   const mostRecentNews = trendingNews.slice(1, 3);
@@ -22,13 +26,14 @@ export function TrendingNews() {
       <div className="flex justify-center items-start gap-8">
         <div>
           {trendingNews &&
-            mainNews.map((item, idx) => {
+            mainNews.map((item) => {
               return (
                 <MainNews
-                  key={idx}
+                  key={item.id}
                   urlImage={item.urlToImage}
                   title={item.title}
                   content={item.description ?? ""}
+                  slug={item.slug!}
                   h={600}
                   w={800}
                 />
@@ -37,12 +42,13 @@ export function TrendingNews() {
         </div>
         <div className="flex flex-col gap-4">
           {trendingNews &&
-            mostRecentNews.map((item, idx) => {
+            mostRecentNews.map((item) => {
               return (
                 <MainNews
-                  key={idx}
+                  key={item.id}
                   urlImage={item.urlToImage}
                   title={item.title}
+                  slug={item.slug!}
                   h={200}
                   w={400}
                 />
@@ -56,8 +62,10 @@ export function TrendingNews() {
         </h2>
         <div className="space-y-4">
           {trendingNews &&
-            trendNews.map((item, idx) => {
-              return <ShortNews key={idx} title={item.title} />;
+            trendNews.map((item) => {
+              return (
+                <ShortNews key={item.id} title={item.title} slug={item.slug!} />
+              );
             })}
         </div>
       </div>
