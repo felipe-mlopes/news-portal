9ff732a7fdbcc6d8aa861c2b1a878d882397c6d7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-07 01:24:20 -0300

    refactor: move to (home) folder, add metada, add margin top and bottom in main tag styles and change category props in CategoryNews components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,23 @@
+import { Metadata } from "next";
+
 import { Header } from "@/view/components/header";
 import { TrendingNews } from "@/view/components/trending-news";
 import { CategoryNews } from "@/view/components/category-news";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 export default function Home() {
   return (
     <div className="h-screen">
       <Header />
-      <main className="flex flex-col lg:block">
+      <main className="flex flex-col space-y-16 lg:block">
         <TrendingNews />
         <div className="space-y-8">
-          <CategoryNews category="política" />
-          <CategoryNews category="economia" />
-          <CategoryNews category="esportes" />
+          <CategoryNews category="business" />
+          <CategoryNews category="economy" />
+          <CategoryNews category="sports" />
         </div>
       </main>
     </div>
