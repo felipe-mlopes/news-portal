b9ae44130d140c9c5c4e41e2852502dc8362a01b
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-07 01:33:42 -0300

    refactor: change getTrendingNews to getNewsByCategory function, add generateDateFormated function, change trendingNews to news variable name and update styles

--- src/view/components/category-news.tsx
+++ src/view/components/category-news.tsx
@@ -1,4 +1,7 @@
-import { getTrendingNews } from "@/models/data-fetching/get-trending-news.model";
+import { getNewsByCategory } from "@/models/data-fetching/get-news-by-category.model";
+
+import { generateDateFormated } from "../utils/generate-date-formated";
+
 import { CardNews } from "./card-news";
 
 interface CategoryNewsProps {
@@ -6,27 +9,34 @@ interface CategoryNewsProps {
 }
 
 export async function CategoryNews({ category }: CategoryNewsProps) {
-  const response = await getTrendingNews();
+  const { dateFrom, dateTo } = generateDateFormated();
+
+  const response = await getNewsByCategory(category, dateFrom, dateTo);
 
   if (!response) {
     return null;
   }
 
-  const trendingNews = response.data.articles;
-  const categoryNews = trendingNews.slice(0, 7);
+  const news = response.data.articles;
+  const categoryNews = news.slice(0, 6);
 
   return (
-    <section className="space-y-4">
-      <h2 className="text-xl font-bold capitalize">{category}</h2>
-      <div className="flex flex-wrap gap-4">
-        {trendingNews &&
+    <section className="px-8 md:px-16 lg:px-32 ">
+      <h2 className="text-2xl font-bold uppercase">{category}</h2>
+      <div className="flex flex-wrap gap-4 py-8 md:justify-center lg:justify-normal border-b-[1px] border-gray-200">
+        {news &&
           categoryNews.map((item, idx) => {
             return (
               <CardNews
                 key={idx}
                 urlImage={item.urlToImage}
                 title={item.title}
+                category={category}
                 slug={item.slug!}
+                author={item.author}
+                content={item.content ?? ""}
+                description={item.description ?? ""}
+                publishedAt={item.publishedAt}
               />
             );
           })}
